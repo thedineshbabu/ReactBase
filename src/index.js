d8fe86e6b7529dd5d3bcea0a60be034020709d0d
Dineshbabu Manoharan <thedineshbabu@hotmail.com>
2021-12-18 16:22:17 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import reactDom from "react-dom";
+
+const App = () => {
+  return <div>React App From Scratch</div>;
+};
+
+reactDom.render(<App />, document.getElementById("root"));
